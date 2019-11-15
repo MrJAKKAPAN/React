b3204e07e5622aef0ca808a7006efaa887e679f2
MrJAKKAPAN <gamegame51130@gmail.com>
2019-11-15 16:10:28 +0700

    commit

---
+++ ant Design/login/src/components/login.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import './css/style.css'
+
+ class Login extends Component {
+    render() {
+        return (
+            <div>
+              dsfsf      
+
+
+
+            </div>
+        )
+    }
+}
+export default Login; 
